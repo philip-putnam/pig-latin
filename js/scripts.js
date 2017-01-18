3c3ad72ed567fc4bbba6fe72399fc1a175e0fa26
philip putnam and Zach Swanson <philip@>
2017-01-18 11:27:43 -0800

    add function to check for alphanumeric values, re-order words starting with consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business logic
 
-var test = "idol";
+var test = "standard";
 
 var isVowel = function(letter){
   var vowels = ["a","e","i","o","u"];
@@ -13,10 +13,25 @@ var isVowel = function(letter){
   return vowelTrue;
 };
 
+var moveCons = function(string) {
+  var subStr = "";
+  var ctr;
+  for (var index = 0; !isVowel(string.charAt(index)); index += 1) {
+      subStr = subStr + string.charAt(index);
+      ctr = index;
+    }
+  subStr = string.slice((ctr+1), string.length) + subStr;
+  return subStr;
+};
+
 var pigTranslate = function(input){
-  if (isVowel(input.charAt(0))) {
+  if (input.match(/[^a-z]/g) === null) {
+    if (!isVowel(input.charAt(0))) {
+      input = moveCons(input);
+    }
     input = input + "ay";
   }
+
     return input;
 };
 
