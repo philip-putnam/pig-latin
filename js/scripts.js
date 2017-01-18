b7282ad0978e2e011dc2f2c1e31bca411ec02eb8
philip putnam and Zach Swanson <philip@>
2017-01-18 09:54:15 -0800

    add functions to check if character is vowel and return input with ay added

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 // Business logic
 
 
-var test = "21%";
+var test = "u";
+
+var isVowel = function(letter){
+  var vowels = ["a","e","i","o","u"];
+  var vowelTrue = false;
+  vowels.forEach(function(vowel) {
+    if (letter === vowel) {
+      vowelTrue = true;
+    }
+  });
+  return vowelTrue;
+};
+
 
 var pigTranslate = function(input){
-  return (input);
-}
+  if (isVowel(input)) {
+    input = input + "ay";
+  }
+    return input;
+};
 
 alert(pigTranslate(test));
